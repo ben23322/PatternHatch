93d3a31f0b525ef90470dc191205034a4adc9bed
Monika <monika@users.noreply.github.com>
2026-08-01 13:06:33 +0800

    v0.1.21: 1x safe mode + matchesFound probe to pinpoint parallel consumption failure

--- src/main/java/tj/patternhatch/TJPatternHatchMod.java
+++ src/main/java/tj/patternhatch/TJPatternHatchMod.java
@@ -21,7 +21,7 @@ public class TJPatternHatchMod implements ILateMixinLoader {
 
     public static final String MODID = "patternhatch";
     public static final String NAME = "Pattern Hatch (TJ)";
-    public static final String VERSION = "0.1.20";
+    public static final String VERSION = "0.1.21";
 
     @Mod.Instance(MODID)
     public static TJPatternHatchMod instance;
@@ -43,6 +43,7 @@ public class TJPatternHatchMod implements ILateMixinLoader {
 
     @Override
     public List<String> getMixinConfigs() {
-        return Arrays.asList("mixins.patternhatch.json", "mixins.patternhatch.nae2.json");
+        return Arrays.asList("mixins.patternhatch.json", "mixins.patternhatch.nae2.json",
+                "mixins.patternhatch.ga.json");
     }
 }
